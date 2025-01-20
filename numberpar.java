3eef5ad03a76e821cecfafc3c2574b9bb315323d
Irving Jose <irvingjoseperezgris137@gmail.com>
2025-01-19 18:49:53 -0600

    Subiendo el archivo de java

---
+++ numberpar.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class numberpar{
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        boolean espar = false;
+        System.out.print("Ingrese un numero:  ");
+        int num = scanner.nextInt();
+        scanner.close();
+        if(num%2==0){
+            espar = true;
+        }
+        if(espar){
+            System.out.println("El numero " + num + " es par");
+        } else{
+            System.out.println("El numero " + num + " es impar");
+        }
+
+        if(num  > 100){
+            System.out.println("El numero es mayor o igual que 100");
+        } else System.out.println("El numero es menor que 100");
+    }
+}
