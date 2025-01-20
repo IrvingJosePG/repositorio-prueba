9b7307f6eba71becbb57b96bcbdd2cd1e1ffe53b
Irving Jose <irvingjoseperezgris137@gmail.com>
2025-01-19 19:26:29 -0600

    Creando copianumberpar.java para despedida

---
+++ copianumberpar.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class numberpar{
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        boolean espar = false;
+        System.out.print("Ingrese un numero:  ");
+        int num = scanner.nextInt();
+        scanner.close();
+        System.out.println("Gracias por el numero.");
+        if(num%2==0){
+            espar = true;
+        }
+        if(espar){
+            System.out.println("El numero " + num + " es par");
+        } else{
+            System.out.println("El numero " + num + " es impar");
+        }
+
+        if(num  > 100){
+            System.out.println("El numero es mayor o igual que 100");
+        } else System.out.println("El numero es menor que 100");
+    }
+  System.out.println("Buena Suerte");
+}
